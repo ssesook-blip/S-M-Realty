68a03f3b246c6a82270edd3163000522a40821bb
ssesook-blip <ssesook@gmail.com>
2026-07-28 20:51:22 -0400

    Create generate_card.py

---
+++ pipeline/generate_card.py
@@ -0,0 +1,31 @@
+import html
+from lib import (
+    clean_title, format_price_card, price_bucket, format_bathrooms, search_blob,
+)
+
+
+def generate_listing_card(listing: dict) -> str:
+    title = clean_title(listing.get("name", ""))
+    slug = listing["slug"]
+    city = listing.get("city", "")
+    sector = listing.get("sector", "")
+    price = format_price_card(listing)
+    bucket = price_bucket(listing)
+    beds = listing.get("room", 0)
+    baths = format_bathrooms(listing)
+    price_num = listing.get("sale_price") or ""
+    image = listing.get("featured_image", "")
+
+    return f'''      <a href="properties/{slug}.html" class="listing reveal" data-city="{html.escape(city)}" data-price="{bucket}" data-beds="{beds}" data-search="{html.escape(search_blob(listing))}" data-price-num="{price_num}">
+        <div class="photo" style="position:relative;">
+          <img src="{image}" alt="{html.escape(title)}" loading="lazy">
+        </div>
+        <div class="listing-body">
+          <div>
+            <div class="listing-price">{html.escape(price)}</div>
+            <div class="listing-addr">{html.escape(sector)}, {html.escape(city)}</div>
+          </div>
+          <div class="listing-meta"><span>{beds} Bed</span><span>{baths} Bath</span></div>
+        </div>
+      </a>
+'''
